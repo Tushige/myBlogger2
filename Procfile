2b6ffaa037255de679c1a023f6a1cc20b898446f
Tushig Ochirkhuyag <Tushig@Tushigs-MacBook-Pro.local>
2017-04-05 19:13:24 -0500

    modified to be heroku deployable

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myblogger.wsgi --log-file -
--- myblogger/wsgi.py
+++ myblogger/wsgi.py
@@ -8,7 +8,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblogger.settings")
 
 application = get_wsgi_application()
+# whitenoise adds capability to serve 'static' files
+application = DjangoWhiteNoise(application)
